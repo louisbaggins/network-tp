66abc3bde60330dc827583802c31549f0e8e85b6
louisbaggins <luis_araujo.c@icloud.com>
2023-05-22 17:32:03 -0300

    Final commit

--- client.cpp
+++ client.cpp
@@ -11,27 +11,81 @@ using namespace std;
 const int MAX_MSG_SIZE = 512;
 const int PORT = 51511;
 
-int main() {
-    // Cria um socket IPv4, TCP
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd < 0) {
-        perror("Erro ao criar socket");
+bool compareStringsIgnoreCase(const string& str1, const string& str2) {
+    if (str1.size() != str2.size()) {
+        return false;
+    }
+    
+    for (size_t i = 0; i < str1.size(); ++i) {
+        if (tolower(str1[i]) != tolower(str2[i])) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int isFileSupported(string& filename) {
+    string extension;
+    size_t dotIndex = filename.find_last_of('.');
+    if (dotIndex != string::npos) {
+        extension = filename.substr(dotIndex + 1);
+    }
+    
+    if (compareStringsIgnoreCase(extension, "txt") || compareStringsIgnoreCase(extension, "c") ||
+        compareStringsIgnoreCase(extension, "cpp") || compareStringsIgnoreCase(extension, "py") ||
+        compareStringsIgnoreCase(extension, "tex") || compareStringsIgnoreCase(extension, "java")) {
         return 1;
     }
+    
+    return 0;
+}
 
-    // Configura o endereço e a porta do servidor
-    struct sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(51511);
+int connect(const char* version, const char *port){
+    if(sizeof(version) > 3){
+        int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (socket_fd < 0) {
+            perror("Erro ao criar socket");
+            return 1;
+        }
 
-    // Conecta ao servidor
-    if (connect(socket_fd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Erro ao conectar-se ao servidor");
-        return 1;
+        struct sockaddr_in serv_addr;
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        serv_addr.sin_port = htons(atoi(port));
+
+        if (connect(socket_fd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+            perror("Erro ao conectar-se ao servidor");
+            return 1;
+        }
+
+        cout << "Conectado ao servidor!" << endl;
+        return socket_fd;
     }
+    else{
+        int socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
+        if (socket_fd < 0) {
+            perror("Erro ao criar socket");
+            return 1;
+        }
 
-    std::cout << "Conectado ao servidor!" << std::endl;
+        struct sockaddr_in6 serv_addr;
+        serv_addr.sin6_family = AF_INET;
+        serv_addr.sin6_addr = in6addr_any;
+        serv_addr.sin6_port = htons(atoi(port));
+
+        if (connect(socket_fd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+            perror("Erro ao conectar-se ao servidor");
+            return 1;
+        }
+
+        cout << "Conectado ao servidor!" << endl;
+        return socket_fd;
+    }
+}
+
+int main(int argc, char* argv[]){
+    int socket_fd = connect(argv[1], argv[2]);
 
     char buffer[MAX_MSG_SIZE];
     string command, file_name;
@@ -46,32 +100,32 @@ int main() {
         }
         else if(command.substr(0, 12) == "select file "){
             file_name = command.substr(12);
-
+            if(isFileSupported(file_name) == 0){
+                cout << file_name << " not valid!" << endl;
+                continue;    
+            }
             ifstream file(file_name);
             if (file.good()){
                 cout << file_name << " selected" << endl;
                 has_file = 1;
             }
             else{
+                cout << file_name << " does not exist" << endl;
                 has_file = 0;
             }
         }
         else if(command == "send file"){
             if(has_file != 1){
-                cout << "no file slected!" << endl;
+                cout << "no file selected!" << endl;
                 continue;
             }
             send(socket_fd, file_name.c_str(), sizeof(file_name), 0);
             ifstream file(file_name);
             streampos fileSize = file.tellg();
-            cout << fileSize << endl;
             while (!file.eof()) {
-                // Ler o próximo pacote do arquivo
-                cout << "Leitura " << fileSize << endl;  
                 file.read(buffer, sizeof(buffer));
                 size_t bytesRead = file.gcount();
 
-                // Enviar o pacote para o servidor
                 if (send(socket_fd, buffer, bytesRead, 0) < 0) {
                     perror("Erro ao enviar dados do arquivo para o servidor");
                     return 1;   
@@ -79,60 +133,19 @@ int main() {
 
                 fileSize -= bytesRead;
             }
-    
+            int bytes_received = recv(socket_fd, buffer, sizeof(buffer), 0);
+            if(bytes_received > 0){
 
-            // Fechar o arquivo
+            }
+
             file.close();
         }
-
-
-        
-
-        // ifstream file(command.c_str(), ios::binary | ios::ate);
-        // if (!file) {
-        //     perror("Erro ao abrir o arquivo");
-        //     return 1;
-        // }
-
-        // // Obter o tamanho do arquivo
-        // streampos fileSize = file.tellg();
-        // file.seekg(0, ios::beg);
-
-        
-        // if (command == "teste"){
-           
-        // }
-        // if (command == "exit") {
-        //     break;
-        // }
-
-        // if(send(socket_fd, command.c_str(), sizeof(command.c_str()), 0) > 0){
-        //     while(recv(socket_fd, buffer, sizeof(buffer), 0) < 0){
-        //         send(socket_fd, command.c_str(), sizeof(command.c_str()), 0);
-        //     }
-        //     std::cout << buffer << std::endl;
-        // }
-
-        // while (fileSize > 0) {
-        //     // Ler o próximo pacote do arquivo
-        //     file.read(buffer, sizeof(buffer));
-        //     size_t bytesRead = file.gcount();
-
-        //     // Enviar o pacote para o servidor
-        //     if (send(socket_fd, buffer, bytesRead, 0) < 0) {
-        //         perror("Erro ao enviar dados do arquivo para o servidor");
-        //         return 1;
-        //     }
-
-        //     fileSize -= bytesRead;
-        // }
-
-        // // Fechar o arquivo
-        // file.close();
+        else{
+            send(socket_fd, "exit", sizeof("exit"), 0);
+            break;
+        }
     }    
-    // Usa o socket...
-    
-    // Fecha o socket quando terminar de usá-lo
+
     close(socket_fd);
     return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -28,53 +28,79 @@ int hasFile(string file_name){
    return 0;
 }
 
-int main(){
-   int socketfd = socket(AF_INET, SOCK_STREAM, 0);
-   if (socketfd < 0){
-      perror("Error on creating socket");
-      return 1;
-   }
-
-   struct sockaddr_in server_addr;
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_port = htons(51511);
-   server_addr.sin_addr.s_addr = INADDR_ANY;
-   
-
-   if(bind(
-      socketfd, 
+int connect(char* version, char* port){
+   if(strcmp(version, "v4") == 0){
+      int socketfd4 = socket(AF_INET, SOCK_STREAM, 0);
+      if (socketfd4 < 0){
+         perror("Error on creating socket");
+         return 1;
+      }
+      struct sockaddr_in server_addr;
+      server_addr.sin_family = AF_INET;
+      server_addr.sin_port = htons(atoi(port));
+      server_addr.sin_addr.s_addr = INADDR_ANY;
+       if(bind(
+      socketfd4, 
       (struct sockaddr*) &server_addr, 
       sizeof(server_addr)
       ) < 0){
        perror("Error on bind");
         return 1;
+      }
+      if (listen(socketfd4, 1) < 0){
+         perror("Erro ao escutar conexões");
+         return 1;
+      }
+
+      return socketfd4;
    }
-   std::cout << "binded!" << std::endl;
-   std::cout<<listen(socketfd, 1) << std::endl;
-   if (listen(socketfd, 1) < 0){
-      perror("Erro ao escutar conexões");
+   else{
+      cout << atoi(port) << endl;
+      struct sockaddr_in6 server_addr;
+      server_addr.sin6_family = AF_INET6;
+      server_addr.sin6_port = htons(atoi(port));
+      server_addr.sin6_addr = in6addr_any;
+      int socketfd = socket(AF_INET6, SOCK_STREAM, 0);
+      if (socketfd < 0){
+         perror("Error on creating socket");
+      return 1;
+      }
+      if(bind(
+      socketfd, 
+      (struct sockaddr*) &server_addr, 
+      sizeof(server_addr)
+      ) < 0){
+         perror("Error on bind");
+         return 1;
+      }
+      if (listen(socketfd, 1) < 0){
+         perror("Erro ao escutar conexões");
+         return 1;
+      }
+      return socketfd;
+      }
+}
+
+
+int main(int argc, char* argv[]){
+   char* version = argv[1];
+   char* port = argv[2];
+   int socketfd = connect(version, port);
+   int newsockfd = accept(socketfd, NULL, NULL);
+   if (newsockfd < 0) {
+      perror("Erro ao aceitar conexão");
       return 1;
    }
-
-   int newsockfd = accept(socketfd, NULL, NULL);
-   std::cout << newsockfd << std::endl;
-   if (newsockfd < 0) {
-        perror("Erro ao aceitar conexão");
-        return 1;
-   }
-
-    std::cout << "Conexão aceita!" << std::endl;
-    char buffer[MAX_MSG_SIZE];
+   cout << "Conexão aceita!" << endl;
+   char buffer[MAX_MSG_SIZE];
 
     while(true){
-      if(buffer == "exit"){
-         std::cout << "connection closed" << std::endl;
+      int bytes_received = recv(newsockfd, buffer, sizeof(buffer), 0);
+      if(strcmp(buffer, "exit") == 0){
+         cout << "connection closed" << endl;
          break;
       }
-      int bytes_received = recv(newsockfd, buffer, sizeof(buffer), 0);
-      if (bytes_received > 0){
-         cout << "Recebido " << buffer << endl;
-      }
+
       string directory = "./server/";
       string filename(buffer);
       string path = directory + filename;
@@ -86,7 +112,6 @@ int main(){
       else{
          file_status = "overwritten";
       }
-      cout << file_status << endl;
       ofstream file;
       file.open(path.c_str());
       if (!file.is_open()) {
@@ -96,17 +121,15 @@ int main(){
       timeout.tv_sec = 5;
       timeout.tv_usec = 0;
       setsockopt(newsockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
-      cout << "Aqui" << endl;
       do {
          bytes_received = recv(newsockfd, buffer, sizeof(buffer), 0);
          if (bytes_received > 0) {
             file.write(buffer, bytes_received);
          }
       } while (bytes_received > 0);
-      timeout.tv_sec = 5;
+      timeout.tv_sec = 0;
       setsockopt(newsockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
 
-      cout << "veio" << endl;
       string file_received = "file " + filename + " " + file_status;
       send(newsockfd, file_received.c_str(), sizeof(file_received), 0);
       cout << file_received << endl;
